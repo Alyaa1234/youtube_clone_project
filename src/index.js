c6126526ea3f270b00e706aed3133886d6b30e77
Alyaa <alyaah131@gmail.com>
2024-03-15 18:03:16 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
+
+import App from './App'
+import './index.css'
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App/>);
+
+
+
+
+
+// مسلسل كوري احبك في حزني 
+//بتعمل عمليه وتغير وشها علشان بتهرب من جوزها
